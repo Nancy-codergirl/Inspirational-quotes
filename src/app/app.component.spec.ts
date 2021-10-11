9c3c32d985569ea1e26353a2594988d017d1339e
Nancy-codergirl <nancymuriithi@student.moringaschool.com>
2021-10-11 06:41:21 +0300

    Change boilerplate code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Quotes'`, () => {
+  it(`should have as title 'quote'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Quotes');
+    expect(app.title).toEqual('quote');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Quotes app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('quote app is running!');
   });
 });
