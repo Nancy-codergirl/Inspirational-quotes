3488b7e6f7b58acaf4caea713b2b6e655fe228c1
Nancy-codergirl <nancymuriithi@student.moringaschool.com>
2021-10-11 07:20:56 +0300

    Add about and project components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
