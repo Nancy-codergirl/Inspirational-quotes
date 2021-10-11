3122a95de439885b8739c034f345a53b267c1798
Nancy-codergirl <nancymuriithi@student.moringaschool.com>
2021-10-11 12:22:50 +0300

    Add default quotes

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Quotes,quotes } from 'src/app/quotes';
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
 })
-export class DetailsComponent implements OnInit {
+export class QuoteDetailsComponent implements OnInit {
+  @Input()quote:Quotes;
 
-  constructor() { }
+  upvote:number = 0;
+  downvote:number = 0;
+  onUpVote(){
+    this.upvote ++;
+
+  }
+ 
+  onDownVote(){
+    this.downvote ++;
+  }
+
+  
+
+  constructor() { 
+    
+    
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
